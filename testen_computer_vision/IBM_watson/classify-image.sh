811704b4697d53eaadab577e7183fdf8c41ce45d
nvanderperren <ik.ben.nas@gmail.com>
2019-03-17 21:33:54 +0100

    deleted tests

--- testen_computer_vision/IBM_watson/classify-image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# variables
-apikey=$1
-imageURL=$2
-version=2018-03-19
-url="https://gateway.watsonplatform.net/visual-recognition/api/v3/classify?url=$imageURL&version=$version"
-
-curl -u "apikey:$apikey" "$url" > classify-image.json
