a62afcc6a6f19f037e53f7b9daf2423d898d2504
nvanderperren <ik.ben.nas@gmail.com>
2019-07-08 19:52:36 +0200

    start draft script training

--- research/scripts/train-test.sh
+++ research/scripts/train-test.sh
@@ -1,10 +1,41 @@
-cat $1 | sed 1d | cut -d , -f2,3 | \
+key=$1
+file=$2
+domain=http://ec2-18-191-252-182.us-east-2.compute.amazonaws.com:8182/iiif/2/
+suffix=/full/pct:50/0/default.jpg
+
+cat $file | sed 1d | cut -d , -f2,5 | \
 while read line
 do
-    url="${line%,*}"
-    else="${line##*,}"
-    echo $url
-    echo $else
+    image="${line%,*}"
+    concept="${line##*,}"
+    url="${domain}${image}${suffix}"
+
+    echo "${url} busy"
+    echo "${concept}"
+
+    curl -X POST \
+    -H "Authorization: Key $key" \
+    -H "Content-Type: application/json" \
+    -d '
+    {
+    "inputs": [
+      {
+        "data": {
+          "image": {
+            "url": "'"${url}"'"
+          },
+          "concepts":[
+            {
+              "id": "'"${concept}"'",
+              "value": true
+            }
+          ]
+        }
+      }
+    ]
+  }'\
+  https://api.clarifai.com/v2/inputs | jq . > ../output/test/${image}.json
+
 
 done
 echo "done"
