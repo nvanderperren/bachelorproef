6075fcdf07c8b71b2e9f5d3aaca4b68a6c5eb0d9
nvanderperren <ik.ben.nas@gmail.com>
2019-07-13 16:25:22 +0200

    create model

--- research/scripts/train/train_model.sh
+++ research/scripts/train/train_model.sh
@@ -8,12 +8,14 @@
 # sh train_model.sh ${api_key} ${model_id} ${iteration}
 #
 # Dependencies: cURL, jq
+# model ids:
+# - thema: 68b9e3c20ae04abbbdc2e3fb974b9a0f
 #
 
 key=$1
 model_id=$2
 iteration=$3
-file=../../output/train/model/train_model_$iteration.json  # change this if you want to store the file anywhere else
+file=../../output/train/model/thema/train_model_$iteration.json  # change this if you want to store the file anywhere else
 
 curl -X POST \
   -H "Authorization: Key $key" \
